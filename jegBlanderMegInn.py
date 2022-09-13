25108bc4bf17c7b4a9f6b7f24c1fe60b1548184a
uwohlen <112326477+uwohlen@users.noreply.github.com>
2022-09-13 16:51:14 +0200

    Create jegBlanderMegInn.py

---
+++ jegBlanderMegInn.py
@@ -0,0 +1,6 @@
+# ved å trykke på navnet ditt istedenfor fork'en, så fant jeg alle dine public repositories
+#crashing the party!!!
+
+#i VSC kan jeg se koden du har lagd i mitt repository, kjøre den som er pulled inn i main, men ikke kjøre den som du har lagd uten å sette i gang en pull request.
+
+print("Jeg blander meg inn!")
